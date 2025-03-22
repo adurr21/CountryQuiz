7821a7d6fb25d4fd369b1515e2dd91a5283830a1
Austin <20157814+adurr21@users.noreply.github.com>
2025-03-22 14:32:58 -0400

    Added basic logging functionality to MainActivity.java

--- app/src/main/java/edu/uga/countryquiz/MainActivity.java
+++ app/src/main/java/edu/uga/countryquiz/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.uga.countryquiz;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +11,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final String LOG_TAG = "edu.uga.countryquiz";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "MainActivity: onCreate() called");
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
